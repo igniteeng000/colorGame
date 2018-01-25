770a5f4b315fe61a4aa57283bc41bee4a9de3ede
igniteeng000 <igniteeng000@gmail.com>
2018-01-25 22:54:05 +0530

    play again

--- js/script.js
+++ js/script.js
@@ -5,6 +5,17 @@
   var colorDisplay = document.getElementById("colorDisplay");
   var messageDisplay = document.querySelector("#message");
   var h1 = document.querySelector("h1");
+  var resetButton = document.querySelector("#reset");
+  resetButton.addEventListener("click",function(){
+    colors = generateRandomColors(6);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (var i = 0; i < squares.length; i++) {
+      squares[i].style.background = colors[i];
+    }
+    h1.style.background = "#232323";
+  })
+
   colorDisplay.textContent = pickedColor;
   for (var i = 0; i < squares.length; i++) {
     squares[i].style.background = colors[i];
@@ -14,6 +25,7 @@
 
     if (clickedColor === pickedColor){
       messageDisplay.textContent = "Correct!";
+      resetButton.textContent = "Play Again?";
       changeColors(clickedColor);
       h1.style.background = clickedColor;
     }else {
